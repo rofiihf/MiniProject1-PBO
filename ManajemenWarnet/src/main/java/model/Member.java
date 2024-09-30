2b5492a82063068f4fe5140643a09847e3794ef1
Rofi Hanif Fauzan <144798359+rofiihf@users.noreply.github.com>
2024-09-30 20:33:55 +0800

    Add files via upload

---
+++ ManajemenWarnet/src/main/java/model/Member.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Member {
+    private String username;
+    private String password;
+    private int durasi;  // Durasi dalam jam
+    private int nomorPC;
+
+    // Konstruktor
+    public Member(String username, String password, int durasi, int nomorPC) {
+        this.username = username;
+        this.password = password;
+        this.durasi = durasi;
+        this.nomorPC = nomorPC;
+    }
+
+    // Getter dan setter
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDurasi() {
+        return durasi;
+    }
+
+    public void setDurasi(int durasi) {
+        this.durasi = durasi;
+    }
+
+    public int getNomorPC() {
+        return nomorPC;
+    }
+
+    public void setNomorPC(int nomorPC) {
+        this.nomorPC = nomorPC;
+    }
+
+    @Override
+    public String toString() {
+        return "Username: " + username + "\n" +
+               "Password: " + password + "\n" +
+               "Durasi: " + durasi + " jam\n" +
+               "Nomor PC: " + nomorPC + "\n" +
+               "-----------------------------------------";
+    }
+}
+
